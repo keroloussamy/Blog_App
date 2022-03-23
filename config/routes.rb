544f12697b37bff9a715f74531c6c08668897619
keroloussamy <keroloussamy98@gmail.com>
2022-03-24 00:16:55 +0200

    Add posts controller and add index and show methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end  
   
 
   # Defines the root path route ("/")
