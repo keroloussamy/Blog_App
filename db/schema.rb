1db942bedccfffcf8047d49138c8db86f908a7cd
keroloussamy <keroloussamy98@gmail.com>
2022-03-29 00:05:32 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_220923) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
+    t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
