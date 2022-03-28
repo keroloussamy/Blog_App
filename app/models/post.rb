bf0a29ccd98df90c2e8889d46600d22215d7f1f8
keroloussamy <keroloussamy98@gmail.com>
2022-03-29 00:28:20 +0200

    add last_five_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
   def update_post_user_counter
     user.update(posts_counter: user.posts.counter)
   end
+
+  def last_five_comments
+    comments.last(5)
+  end
 end
