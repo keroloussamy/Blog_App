ccc911560434813c1201c60d5aaaea1edbf33c9c
keroloussamy <keroloussamy98@gmail.com>
2022-03-29 00:29:25 +0200

    add update_post_like_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user, foreign_key: :users_id
   belongs_to :post, foreign_key: :posts_id
+
+  after_save :update_post_like_counter
+
+  def update_post_like_counter
+    post.update(likes_counter: post.likes.counter)
+  end
 end
