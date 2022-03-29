a38bd0da0edd53a292df4d45db3a8b27b6af7005
keroloussamy <keroloussamy98@gmail.com>
2022-03-30 00:33:21 +0200

    Add class_name: 'User' option to the relation classes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author, foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
 
   after_save :update_post_comments_counter
