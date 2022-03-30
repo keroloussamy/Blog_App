19581b81c1b1ff09b011af51987755fb7f9bb343
keroloussamy <keroloussamy98@gmail.com>
2022-03-30 23:04:43 +0200

    update routes to add the rest of the urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users do
+    resources :posts
   end  
   
 
