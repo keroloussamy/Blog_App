aeca8d9b25bb52dcc8e76702bce1bf7017c7b64c
keroloussamy <keroloussamy98@gmail.com>
2022-03-30 23:05:50 +0200

    Add new and create posts methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    redirect_to user_posts_path(@user) # users/1/posts
+    # redirect_to user_path(@user) # users/1/
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
