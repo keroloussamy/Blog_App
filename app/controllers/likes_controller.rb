ef92af93ade688ee39f2aa0158671ca0d534610e
keroloussamy <keroloussamy98@gmail.com>
2022-04-01 00:16:16 +0200

    Add flash message

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,7 @@ class LikesController < ApplicationController
     like = current_user.likes.new
     like.post_id = params[:post_id]
     like.save
+    flash[:success] = 'Like added successfully'
     redirect_to user_posts_path
   end
 end
