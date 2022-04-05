e4e3f39da8fbbb12cf77f25747e090c7076156f0
keroloussamy <keroloussamy98@gmail.com>
2022-04-05 19:40:59 +0200

    Update ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,9 +31,9 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
-
-    can :manage, Post, user_id: user.id
-    can :manage, Comment, user_id: user.id
+    return unless user.present?
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
     return unless user.admin?
     can :manage, :all
   end
